0ea27e33329eda8194f393f41c0d90f69308f73c
codecheap2019 <codecheap2019@gmail.com>
2020-04-27 22:44:24 +0530

    Hgdgd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const app = angular.module("app", []);
-
-  app.controller("MainCtrl", function($scope) {
-    $scope.heading = "AngularJS";
-    $scope.text = "Be nice to other people; they outnumber you 5.5 billion to one. ";
-  });
+const nav = new Vue({
+  el: '#nav', 
+  data: {
+    store_name: process.env.name
+  }
+});
