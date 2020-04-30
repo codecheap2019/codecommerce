94133631753b8d7a709188b24e83acb44d99bf43
codecheap2019 <codecheap2019@gmail.com>
2020-04-30 18:41:00 +0530

    Bcf

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const nav = new Vue({
     cart: []
   }
 });
+
+const carouselExampleIndicators = new Vue({
+  el: '#carouselExampleIndicators', 
+  data: {
+    img1: 'images/video_bg.jpg', 
+    img2: 'images/video_bg.jpg', 
+    img3: 'images/video_bg.jpg'
+  }
+});
